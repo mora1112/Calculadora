7c10a2d9a07433af90ae5dd4d9fa4320a8eeb2c3
mora1112 <anthonymora1114@gmail.com>
2023-11-14 20:04:51 -0600

    Se crea la funcion factorial en la calculadora cientifica

--- src/com/calculadora/logicanegocio/CalculadoraCientifica.java
+++ src/com/calculadora/logicanegocio/CalculadoraCientifica.java
@@ -58,5 +58,14 @@ public class CalculadoraCientifica extends CalculadoraBasica{
         return Math.sqrt(numero);
     }
     
+    public int factorial(int numero){
+        if(numero==1){
+            return 1;
+        }else{
+            return numero*factorial(numero-1);
+        
+        }
+    }
+    
 
 }
